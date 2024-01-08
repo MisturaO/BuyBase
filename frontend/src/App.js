111553c35b8c6a3825d29d0adf42b4756c70fb73
MisturaO <misturaojulari91@gmail.com>
2024-01-08 14:22:39 +0100

    Feature component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Footer } from './Footer';
 import { StyleSheet, css } from 'aphrodite';
+import { FeaturedItem } from './FeaturedItem';
 
 function App() {
   return (
     <div className="App">
+      <FeaturedItem />
       <Footer />
     </div>
   );
