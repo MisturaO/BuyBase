e6228a23338fd49a84ff150f65ad9299131c9ef6
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2024-01-12 03:16:32 +0100

    (feat) -> create featured section

---
+++ frontend/src/components/sections/featured/FeaturedSection.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FeaturedSectionWrapper } from './featuredsection.styled';
+import FeaturedProductCard from '../../reusable/cards/FeatureProductCard';
+import { productdata } from './constant';
+
+const FeaturedSecton = () => {
+  return (
+    <FeaturedSectionWrapper className='featured'>
+      {productdata.map((item, i) => {
+        return (
+          <FeaturedProductCard
+            key={`product_card${i}${item.image}`}
+            product={item}
+          />
+        );
+      })}
+    </FeaturedSectionWrapper>
+  );
+};
+
+export default FeaturedSecton;
