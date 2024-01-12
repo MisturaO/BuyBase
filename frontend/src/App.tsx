2cc7794cb96cf4b11ebeb37a3aa5baf326dfa157
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2024-01-12 03:19:32 +0100

    (chore) -> update, link and refactor app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Route';
+
+function App() {
+  return (
+    <div className='App'>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
